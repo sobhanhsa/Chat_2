9f30de3fb305fc2b532bffc29503b0d3dc9a9bce
sobhanhsa <sobhanb7b7@protonmail.com>
2022-09-07 14:58:25 +0430

    passchanger

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = new express
-const {apLogin, apSign} = require('./controller.js')
+const {apLogin, apSign, apChange} = require('./controller.js')
 app.use(express.json())
 app.post('/',apLogin) 
-app.post('/sign',apSign)  
+app.post('/sign',apSign) 
+app.post('/changepass',apChange) 
 app.listen(8080, () => console.log("server is listennig on 8080 port"))
--- controller.js
+++ controller.js
@@ -8,18 +8,23 @@ mongoose.connect('mongodb://localhost/loginapp', () => {
 
 let users = []
 
-userFinder()
+userFinder() 
 
 async function userFinder() {
     users = await usermodel.find()
     console.log(users)
+}
+async function passChanger(n, npass) {
+    tuserid = users[n]._id
+    users = await usermodel.find({_id : tuserid}).update({pass: npass})
+    console.log(users) 
 }
 async function increate(tuser) {
     try {
         users = await usermodel.create({ name: tuser.name, pass: tuser.pass, email: tuser.email })
     } catch (e) {
         console.log(e.message)
-    } 
+    }
 }
 //email validate
 let emailRegex = /^[-!#$%&'*+\/0-9=?A-Z^_a-z{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/;
@@ -27,20 +32,20 @@ let isEmailValid = (email) => {
     if (!email)
         return 1;
 
-    if(email.length>254)
+    if (email.length > 254)
         return false;
 
     let valid = emailRegex.test(email);
-    if(!valid)
+    if (!valid)
         return false;
 
-   
+
     let parts = email.split("@");
-    if(parts[0].length>64)
+    if (parts[0].length > 64)
         return false;
 
     let domainParts = parts[1].split(".");
-    if(domainParts.some(function(part) { return part.length>63; }))
+    if (domainParts.some(function (part) { return part.length > 63; }))
         return false;
 
     return true;
@@ -74,9 +79,9 @@ const apSign = (req, res) => {
         return res.send("please enter the pass")
     }
     isEmailValid = isEmailValid(muser.email)
-    if(isEmailValid === 1) {
+    if (isEmailValid === 1) {
         res.send("please enter the email")
-    }else if ( isEmailValid === false) {
+    } else if (isEmailValid === false) {
         res.sedn("incorrect email")
     }
     const repetitive = users.find(element => element.name === muser.name)
@@ -88,8 +93,31 @@ const apSign = (req, res) => {
         return res.send("repetitive name")
     }
 }
-
+const apChange = (req, res) => {
+    const muser = req.body
+    if (!muser.email) {
+        return res.send("please enter the email")
+    }
+    if (!muser.pass) {
+        return res.send("please enter the pass")
+    }
+    if (!muser.newpass){
+        return res.send("please enter the new pass")
+    }
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].email === muser.email) {
+            if (users[i].pass === muser.pass) {
+                passChanger(i, muser.newpass)
+                return res.send(`welcome ${users[i].name} your pass changed to 
+                 ${muser.newpass}`)
+            }
+            return res.send(`incorrect pass for ${users[i].name} user`)
+        }
+    }
+    res.send("the considered email not found")
+}
 module.exports = {
     apLogin,
-    apSign
+    apSign,
+    apChange
 }  
